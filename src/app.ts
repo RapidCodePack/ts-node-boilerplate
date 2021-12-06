f1ca2bacbc5e8cc7ad624e043dd02690be0b98bc
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-12-06 19:45:05 +0700

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// This is an example for importing
+import { helloWorld } from "./constants"
+
+console.log(helloWorld)
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+// This is an example for exporting
+export const helloWorld = "Hello World!"
